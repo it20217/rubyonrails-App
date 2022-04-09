c80c65d38848182f2928aa61abaf2cbb744d933a
Igor Tokarev <igor.tokarev@outlook.com>
2022-04-09 23:12:36 +0200

    CI/CD create

--- deploy.sh
+++ deploy.sh
@@ -23,17 +23,17 @@ fi
 # Create a container called node app that is available on port 8443 from our docker image
 docker create -p 8446:8446 --name ruby_app $IMAGE_NAME
 
-# Write the private key to a file
-echo $PRIVATE_KEY > privatekey.pem
+# # Write the private key to a file
+# echo $PRIVATE_KEY > privatekey.pem
 
-# Write the server kev to a file
-echo $SERVER > server.crt
+# # Write the server kev to a file
+# echo $SERVER > server.crt
 
-# Add the private key to the node app docker container
-docker cp ./privatekey.pem node_app:/privatekey.pem
+# # Add the private key to the node app docker container
+# docker cp ./privatekey.pem node_app:/privatekey.pem
 
-# Add the server kev to the node app docker container
-docker cp ./server.crt node_app:/server.crt
-
+# # Add the server kev to the node app docker container
+# docker cp ./server.crt node_app:/server.crt
+cd rubyonrails-App
 # start the node app container
-docker start node_app
+bin/rails server -p 8446
